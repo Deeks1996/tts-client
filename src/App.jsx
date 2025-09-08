aa7915cefb0e4937939328639115e381a0b8a52c
Deeksha Shetty <shettydeeksha810@gmail.com>
2025-09-08 16:57:54 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import TTSPage from "./pages/TTS";
-// import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TTSPage />} />
-      {/* <Route path="/register" element={<Register />} /> 
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/tts" element=
       {<ProtectedRoute>
           <TTSPage />
         </ProtectedRoute>
-      } />*/}
+      } />
     </Routes>
   );
 }
